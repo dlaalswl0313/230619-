2c317c51048f7f2de44d8678a0972db41521be88
dlaalswl0313 <dlaalswl0313@naver.com>
2023-06-21 12:19:14 +0900

    230621

--- 230620/static/js/script_array5.js
+++ 230620/static/js/script_array5.js
@@ -75,7 +75,10 @@
 
 let num = new Array(); //화면에 표시되는 숫자 저장 배열
 let board=new Array(); //숫자가 출력될 위치 저장 배열
+
 let show=false; //start 버튼 클릭 유무-start 버튼을 누르면 기능이 작동하도록
+let cmp_num=new Array();//1번 숫자와 그 다음 숫자를 클릭했을때 비교하기위한배열
+let choice=new Array(); //클릭한 숫자가 서로 같지않을때 숫자가 사라지는데 알맞은 숫자도 함께 사라진다. 그걸 해결하려고 하는 변수임.(숫자가 출력될 위치 저장배열)
 
 
 function init(){ //초기화
@@ -102,32 +105,33 @@ function init(){ //초기화
         }
     }   
 }
-window.onload=function(){
+    window.onload=function(){
     init(); //초기화 함수 실행
     //id가 start인 버튼 태그 가져오기
     let start = document.getElementById("start");
     //버튼 태그에 클릭 이벤트 등록하기(실행함수는 play 함수)->버튼을 클릭하기 전까지는 화면에 숫자가 보이지않음
     start.addEventListener("click",play);
 
-   let pic = document.getElementsByClassName("picture");//클래스이름이picutre인것을 가져와서 
-   for( var i=0; i<pic.length; i++){ //각각의 태그에 숫자를 넣어준다 
+    let pic = document.getElementsByClassName("picture");//클래스이름이picutre인것을 가져와서 
+    for( var i=0; i<pic.length; i++){ //각각의 태그에 숫자를 넣어준다 
         pic[i].addEventListener("click",same_search);//board는 위치 지정, td객체에 클릭을 하면 same_search 함수 실행하라.
+        }
     }
-}
-function play(){ //버튼을 누르면 적용되는 함수 
+    function play(){ //버튼을 누르면 적용되는 함수 
     let pic = document.getElementsByClassName("back");
     for( var i=0; i<pic.length; i++){
         pic[board[i]].innerHTML = num[i%4];
     }
     setTimeout(function(){
-        let pic = document.getElementsByClassName("back");
+        let pic = document.getElementsByClassName("back");//숫자를 숨겨준다
         for(var i=0; i<pic.length; i++){
-            pic[i].style.display="none";//자바스크립트에서 스타일을 줄수있다.단, 모든걸 사용할수없다. -> 해당태그.style.display="문자열";
+            pic[i].style.display="none";//자바스크립트에서 스타일을 줄수있다. 단, 모든걸 사용 할 수 없다. -> 해당태그.style.display="문자열";
+            //선택한 숫자 모두 화면에 표시하지 말아라.
         }
         show = true; //start 버튼 클릭 했다는 의미 <->false 면 start 버튼을 클릭하지 않았다. 
-     },2000);//2초 뒤, 한 번만 실행하라.
-}
-function  same_search(){ //칸을 눌러도 알림창이 뜨고, 숫자를 눌러도 알림창이 뜬다 , 그러나 start 버튼을 누르면 알림창이 뜨지 않는다. 
+      },2000);//2초 뒤, 한 번만 실행하라.
+    }
+    function  same_search(){ //칸을 눌러도 알림창이 뜨고, 숫자를 눌러도 알림창이 뜬다 , 그러나 start 버튼을 누르면 알림창이 뜨지 않는다. 
     if(!show){ //show 변수가 false라면 not연산에 의해 true가 작동, show변수가 true라면 not연산에 의해 false가 작동한다.
         alert("start 버튼을 클릭해주세요.");
         return; //start 버튼을 클릭하지 않았으면 same_search함수를 실행시키지않는다.
@@ -142,8 +146,41 @@ function  same_search(){ //칸을 눌러도 알림창이 뜨고, 숫자를 눌�
         즉 td라는 객체에 의해 same_search함수가 실행 된 것이다. 
         same_search 함수 안에서 this(자기자신)이라고 사용하면 same_search함수를 실행시킨 td태그를 의미한다.
     */
+    /*
+     자식태그 가져오는 방법
+      children -모든 자식태그를 htmlcollection배열로 가져온다.
+      childnodes - 모든 자식태그를 nodeList의 배열로 가져온다.
+      firstChild - 첫번째 자식 태그
+      lastChild  - 마지막 자식 태그
+    */  
     //td안에있는 span 태그를 제어해야한다. td 태그의 자식은 span이다.
     //var child = this.firstChild;자바스크립트에서 자식을 데려오는 방법
-    var child =this.childNodes[0]; //결과값 [object NodeList], td태그 밑에 있는 모든 자식들을 배열로 저장한다. td의 첫번째 자식은 0번 인덱스
-      child.style.display="inline";//var child =this.childNodes[0]->결과값[object HTMLSpanElement]\var child = this.firstChild->결과값 [object HTMLSpanElement]                                    
+    
+    var child =this.children[0]; //결과값 [object NodeList], td태그(this)밑에 있는 모든 자식들을 배열로 저장한다. td의 첫번째 자식은 0번 인덱스
+    child.style.display="inline";//var child =this.childNodes[0]->결과값[object HTMLSpanElement]\var child = this.firstChild->결과값 [object HTMLSpanElement]
+    //alert(child.innerText); 숫자 비교해서 맞추기 \첫번째 선택한 숫자와 그 다음에 선택한 숫자와 비교를 해야하니 숫자들을 저장해야한다
+    
+    let span = document.getElementsByClassName("back");//숫자가 같지 않은 경우 숫자를 숨겨준다
+        for(var i=0; i<span.length; i++){ //모든 숫자가 삭제되지 않게하기위함
+            if(span[i] === child){ //각각의 태그마다 고유한 명칭을 가짐 그러니 서로 비교가 가능하다
+                choice.push(i); //클릭한 td의 span태그 인덱스를 배열에 저장한다. 
+            }
+    }
+    cmp_num.push(parseInt(child.innerText)); //배열에 숫자 2개가 저장되어있다면 비교, 클릭한 숫자 배열에 저장
+    
+    if(cmp_num.length == 2){
+        if(cmp_num[0] == cmp_num[1]){
+        cmp_num = new Array();//숫자가 같은 것도 배열초기화
+        choice = new Array();//같은 숫자 나와도 초기화
+     }else{ 
+        setTimeout(function(){
+         cmp_num = new Array();//숫자가 달라도 배열초기화
+         let pic = document.getElementsByClassName("back");//숫자가 같지 않은 경우 숫자를 숨겨준다
+         for(var i=0; i<choice.length; i++){
+            pic[choice[i]].style.display="none"; //index=choice에 있는걸 없애라.
+         }
+         choice= new Array();
+      },500); //0.5s,한 번 실행
+     }                                  
+   }
 }
