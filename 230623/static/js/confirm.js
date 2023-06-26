33ecf204fb133835251486f105738589070664cb
dlaalswl0313 <dlaalswl0313@naver.com>
2023-06-26 15:05:30 +0900

    230626

---
+++ 230623/static/js/confirm.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    var drwNo = document.querySelector("");
+    var btnDefault = document.querySelector("#btnDefault");
+    var btnSearch = document.querySelector("#btnSearch");
+
+    
+}
