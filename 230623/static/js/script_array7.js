67e37b0627e266ba87863e6e8a32a5ed4489d771
dlaalswl0313 <dlaalswl0313@naver.com>
2023-06-26 10:09:13 +0900

    230626

--- 230623/static/js/script_array7.js
+++ 230623/static/js/script_array7.js
@@ -1,97 +1,123 @@
 window.onload=function(){
-    //화면이 전부 운영 되면 시작하는 함수
-    var icon=document.getElementsByClassName("strapIcon");
-    icon[0].addEventListener("click",open_close);   
+    // 화면이 전부 로딩 되면 시작하는 함수
+        var icon = document.getElementsByClassName("strapIcon");    
+        icon[0].addEventListener("click", open_close);
     
-    content=document.querySelector("#content");
+        content= document.querySelector("#content");
+        //파일 선택한걸 화면에 불러오기
+        var file = document.querySelector("#lotto"); //css에서의 id가 lotto인걸 가져오기
+        file.addEventListener("input", function(e){
+            let target = e.target;//선택된 파일참조
+            let files = target.files;//선택 된 파일은 배열의 형식으로 저장된다. 
+            //첫번째 파일 참조를 해야 내가 선택한 파일을 찾을 수 있다.
+            //alert(files[0]); 파일선택하면 알림창이 뜬다.
+            let reader=new FileReader();
+            reader.addEventListener("load", function(e){
+                alert(reader.result);
+            });
+            reader.readAsText(files[0]);
+        });
 }
-window.onresize=function(){ //브라우저 화면의 너비를 변경했니?
-    var wt=window.innerWidth;
-    if(wt > 786){//너비가 786보다 작을경우,menu_icon을 누르면 menu_list가 펼쳐지는데 웹브라우저크기가 변하면 펼친게 줄어들게하고싶다
+window.onresize=function(){
+    var wd = window.innerWidth; 
+    if( wd > 786){//너비가 786보다 작을경우,menu_icon을 누르면 menu_list가 펼쳐지는데 웹브라우저크기가 변하면 펼친게 줄어들게하고싶다
         var list = document.getElementsByClassName("menu_list")[0];
         //열려있을때 웹페이지 크기를 키우면 열린 리스트목록이 안보이도록
-        list.style.display="none"; //안보인다
-        list.dataset.show=1;      
+        list.style.display="none";
+        list.dataset.show='1';
     }
 }
-function open_close(){ 
-//menu_icon을 누르면 menu_list가 펼쳐지는데 웹브라우저크기가 변하면 펼친게 줄어들게하고싶다
+function open_close(){
     var list = document.getElementsByClassName("strapIcon")[0].nextSibling;
-    var show = list.dataset.show; //show 에는 1이 들어간다.
-    
+    //menu_icon을 누르면 menu_list가 펼쳐지는데 웹브라우저크기가 변하면 펼친게 줄어들게하고싶다
+    var show = list.dataset.show;
     if(show==1){
-        list.style.display="block"; //보인다
-        list.dataset.show=0; 
+        list.style.display="block"; //보인다.
+        list.dataset.show='0';
     }else{
-        list.style.display="none"; //안보인다
-        list.dataset.show=1; 
-    }  
-    // var list=this.nextSibling;
-    // var isActive=list.classList.contains("list_active");
-    
-    // if(isAcitve)
-    //     list.classList.toggle("list_active");//toggle=삭제시킴
+        list.style.display="none";//안보인다.
+        list.dataset.show='1';
+    }
+
+    // var isActive = list.classList.contains("list_active");
+    // if(isActive)
+    //     list.classList.toggle("list_active");//toggle 삭제
     // else
     //     list.classList.add("list_active");
-
 }
+
 let content=null;
-function win_confirm(){ //당첨확인 버튼을 누르면 자바스크립트 내 해당하는 함수로 이동한다
-    alert("당첨확인");
+
+function win_confirm(){//당첨확인 버튼을 누르면 자바스크립트 내 해당하는 함수로 이동한다
+    alert("당첨확인 클릭");
 }
-function make_num(){ //번호생성부분, 랜덤숫자이용해서번호등장
- var out="<table class='makeTable'>";
+
+function make_num(){ //번호생성부분,랜덤숫자이용해서번호등장
+
+    var out="<table class='makeTable'>";
+
     for(var n=1; n<=5; n++){
-      var lucky_num = new Array();
-      lucky_num.push(Math.floor(Math.random()*45)+1);
-       for(var i=1;i<6;i++){
-         var num=Math.floor(Math.random()*45)+1;
-         if ( lucky_num.indexOf(num) == -1){ 
-            lucky_num.push(num);
-         }else{
-             i--; 
-         }
-    }
-    lucky_num.sort(function(a,b){return a-b;}) //정렬 내림차순
-    //lucky_num.sort(function(a,b){return b-a;}) 정렬 오름차순
-    
-    //로또 숫자를 태그에 담아주기 
-    out += "<tr>";
-    out += "<td class='numTd'>"+n+"</td>"
-    for(var i=0; i<lucky_num.length; i++){
-        out += "<td class='numTd'>"+lucky_num[i]+"</td>";
-    }
-    out += "</tr>";
-    //짝수, 홀수 개수, 총합 출력
-    var even=0; odd=0; //even 짝 odd 홀
-    var total=0;
-     for(var i=0; i<lucky_num.length; i++){
-         total += lucky_num[i];
-         if(lucky_num[i]%2===0){
-            even++;
-         }else{
-            odd++;
-         }
-     }
-     //산술적 복합성 값 구하기 
-     for(i=lucky_num.length-1;i>=1;i++){ 
-          for(var k=i-1; i>=0; i--){
-            ac.push(lucky_num[i] - lucky_num[k]);
+        let lucky_num = new Array();
+        
+        lucky_num.push(Math.floor(Math.random()*45)+1);
+        for(var i=1; i<6;i++){
+            var num = Math.floor(Math.random()*45)+1;
+            if(lucky_num.indexOf(num) == -1 ){
+                lucky_num.push(num);
+            }else{
+                i--;
+            }
         }
-     }
-     out += "<td colspan='7'>"+
-     "총합: "+total+"   "+//띄어쓰기를 위한 ""
-     "홀/짝:"+odd+"/"+even+"</td>";
+        lucky_num.sort(function(a,b){return a-b;})
+        //{return b-a;}) 정렬 오름차순
 
-    }//변수 n 5번 반복하는 for문 끝
-     out += "</table>";
-    
-     content.innerHTML=out;
+        //로또 숫자를 태그에 담아주기
+        out += "<tr>";
+
+        out+="<td class='numTd'>"+n+".</td>";
+        for(var i=0; i<lucky_num.length; i++){
+            out += "<td class='numTd c"+(parseInt(lucky_num[i]/10))+"'>"+lucky_num[i]+"</td>";
+        }
+        out += "</tr>";
+
+        //짝,홀 갯수, 총합 
+        var even=0, odd=0;  // even 짝, odd 홀
+        var total=0;
+        for(var i=0; i<lucky_num.length; i++){
+            total += lucky_num[i];
+            if( lucky_num[i]%2 === 0 ){
+                even++;
+            }else 
+                odd++;
+        }
+
+        // 산술적 복합성 값 구하기
+        var ac = new Array();
+        for(var i=lucky_num.length-1; i>=1; i--){
+            for(var k=i-1; i>=0; i--){ 
+                var tmp = lucky_num[i]-lucky_num[k];
+                if(ac.indexOf(tmp) == -1) //중복숫자제외
+                    ac.push(tmp);
+            }
+        }
+        //역대 당첨 번호와 비교하기
+
+        
+        out += "<td colspan='7'>"+
+        "총합 : "+total+"  "+
+        "홀/짝 : "+odd+"/"+even+"</td>";
+
+    } // 5번 반복하는 for문 끝
+    out += "</table>";
+
+    //출력
+    content.innerHTML = out;
 }
 
-function num_count(){
-
+function num_count(){   
+    alert("출현횟수");
 }
+
 //배열, 실행할 함수 내용을 직접 써서 사용가능 -> 기존에는("사용하고자하는기능",사용하고자하는기능의 함수)
 /*
 아이콘 클릭하면 menu_list가 보이고 안보이고 >>
