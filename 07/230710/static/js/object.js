3aa7cec77aff42777725474689aaa5c9addda7f1
dlaalswl0313 <dlaalswl0313@naver.com>
2023-07-10 10:36:52 +0900

    script_object

--- 07/230710/static/js/object.js
+++ 07/230710/static/js/object.js
@@ -1,6 +1,6 @@
 //HTML에는 js 연결만 해준 상태
 window.onload=function(){
-    // var body = document.getElementsByTagName("body");//body 태그 가져옴
+    var body = document.getElementsByTagName("body");//body 태그 가져옴
     // body[0].innerHTML="<div>오늘 비와?</div>";// body[0]-> getElements tag 가 하나만 가져오는게아냐
     // // body[0].innerText="<div>오늘 비와?</div>";   
 
@@ -15,14 +15,19 @@ window.onload=function(){
 }
 //<button onclick()="exit()">브라우저 닫기</button>
 function exit(){
-    //브라우저객체
+    //브라우저 객체
     window.close();
 }
+
 // <button onclick="winopen()">새 창 열기</button>
-function winopen(){
-    // window.open(); 새 탭 생김
-    window.open("http://www.naver.com","_blank","width:200, height=300 top=400,left=500");
-    //window.open("경로(주소)","새 창의 이름 또는 타깃(어디에?)","너비, 높이, 위,아래")
-    //"새 창의 이름 또는 타깃(어디에?) ex) "_parent","_self" : 부모창(현재창)  |  "_blank" ,"" : 새 창이 뜬다(너비,높이 지정해야한다//너비,높이 지정안하면 새창으로안뜸) 
-    //원하는 방향으로 창을 띄울수있음, (ex)회원가입/인증 창
+function winopen(){ //윈도우 객체 생성
+    // // window.open(); 새 탭 생김
+    // window.open("http://www.naver.com","_blank","width:200, height=300 top=400,left=500");
+    // /*
+    //  window.open("경로(주소)","새 창의 이름 또는 타깃(어디에?)","너비, 높이, 위,아래")
+    // "새 창의 이름 또는 타깃(어디에?)" 원하는 방향으로 창을 띄울수있음, (ex)회원가입/인증 창
+    //  ex) "_parent","_self" : 부모창(현재창)  |  "_blank" ,"" : 새 창이 뜬다(너비,높이 지정해야한다//너비,높이 지정안하면 새창으로안뜸) 
+    // */
+    window.open("./child.html","_blank","width:100, height:200");
+    //script_html에서 golive-> 새 창 열기 버튼->child.html 창이 뜬다.-> hey ghost show yourself
 }
