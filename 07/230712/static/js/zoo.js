10234b98a89f942e3a7d0f65f9a1788db275e1af
dlaalswl0313 <dlaalswl0313@naver.com>
2023-07-12 13:40:34 +0900

    230712

---
+++ 07/230712/static/js/zoo.js
@@ -0,0 +1,19 @@
+window.onload=function(){
+ //폼 이랑 버튼 불어와서
+ //
+}
+
+// <form id="searchform" name="searchform" method="get" action="index.html">
+// <div>
+//     <input type="text" id="s" placeholder="검색입력"></input>
+//     <button class="srh_button">검색</button>
+// </div>
+// </form>
+
+function close(){
+    //아이콘 x 불러와야하나,
+//     <div class="search_bar" style="display: none;">
+//     <div class="search_close">
+//     <i class="bi bi-x-lg"></i>
+// </div>
+}
