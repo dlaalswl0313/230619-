9df3eac2b8ad75656f5313c37efe59fe77598ab3
dlaalswl0313 <dlaalswl0313@naver.com>
2023-07-14 10:17:26 +0900

    230714

--- 07/230712/static/js/zoo.js
+++ 07/230712/static/js/zoo.js
@@ -15,15 +15,34 @@
 </div> */
 
 window.onload=function(){
-    var search_bt = document.getElementsByClassName("search_button");
-    search_bt[0].addEventListener("click",function(){ /*ClassName 중 하나를 골라내야하니까 */
-        var bar=document.querySelectorAll(".search_bar")[0];
-        bar.style.display="block";
-    });
-    var search_close=document.getElementsByClassName("search_close");
-    search_close[0].addEventListener("click",function(){
-        var bar=document.querySelectorAll(".search_bar")[0];
-        bar.style.display="none";
-    });
+    
+  var pre = document.getElementsByClassName("prev")[0];
+  var next = document.getElementsByClassName("next")[0];
+  pre.addEventListener("click", function(){
+      var slideview = document.getElementsByClassName("eventslide")[0]; //ul태그
+      slideview.style.transform="translate3d(0px, 0px, 0px)";
+      // var now_left = slideview.style.left.split("px")[0]; //현재 left값가져오기
+      // if( now_left == 0 ) return; // 첫번째 li가 화면에 보이면 이동안되게
+      // var left = Number(now_left) + 312; // li 하나당 312px 이기 때문에 
+      // slideview.style.left=left+"px";
+  });
+  next.addEventListener("click",function(){
+      var slideview = document.getElementsByClassName("eventslide")[0]; //ul태그
+      slideview.style.transform="translate3d(-936px, 0px, 0px)";
+      // var now_left = slideview.style.left.split("px")[0]; //현재 left값가져오기
+      // if( now_left == -936 ) return; // 마지막 li가 화면에 보이면 이동안되게
+      // var left = now_left - 312; // li 하나당 312px 이기 때문에 
+      // slideview.style.left=left+"px";
+  });
+  
+  var search_bt = document.getElementsByClassName("search_button");
+  search_bt[0].addEventListener("click",function(){
+      var bar = document.querySelectorAll(".search_bar")[0];
+      bar.style.display="block";
+  });
+  var search_close = document.getElementsByClassName("search_close");
+  search_close[0].addEventListener("click",function(){
+      var bar = document.querySelectorAll(".search_bar")[0];
+      bar.style.display="none";
+  });
 }
-
