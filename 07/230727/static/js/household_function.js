50d313c8ab95e22de95eb7792e8b4fbb7939ff4a
dlaalswl0313 <dlaalswl0313@naver.com>
2023-07-27 15:22:11 +0900

    230727

--- 07/230727/static/js/household_function.js
+++ 07/230727/static/js/household_function.js
@@ -1,22 +1,27 @@
-//여기는 전역변수 정의한 곳입니다.
-let isIncome=false;//수입이냐 지출이냐 false 면 지출 , true면 수입
-let way="";//현금이냐 계좌냐 카드냐
+// household_function.js
 
-//전역변수 정의한 곳 ::::끝
+//여기는 전역변수 정의한 곳입니다.
+
+let isIncome=false; // 수입 이냐 지출이냐  false면 지출 , true면 수입
+let way="";  // 현금이냐 계좌냐 카드냐
+let keep=new Array(); // 가계부 내역 객체가 저장될 배열
+
+// 전역변수 정의 한곳 끝
+
 function two_digit(num){
-    return num<10 ? "0"+num : num ;
+    return  num<10 ? "0"+num : num ;
 }
 
 window.onload=function(){
     var today = new Date();
-    var year = today.getFullYear();
-    var month = two_digit(today.getMonth()+1);
-    var date = two_digit(today.getDate());
-    var hour = two_digit(today.getHours());
-    var minute = two_digit(today.getMinutes());
-
-    var wDate = document.querySelector("#wDate");
-    wDate.value=year+"-"+month+"-"+date+" "+hour+":"+minute;
+    var year = today.getFullYear(); //년도
+    var month = two_digit(today.getMonth()+1); //월
+    var date = two_digit(today.getDate());  // 일
+    var hour = two_digit(today.getHours()); //시간
+    var minute = two_digit(today.getMinutes()); //분
+    
+    var wdate = document.querySelector("#wdate");
+    wdate.value=year+"-"+month+"-"+date+" "+hour+":"+minute;
 
     var cate = document.querySelector("#wcategory");
 
@@ -27,7 +32,7 @@ window.onload=function(){
         cate.appendChild(opt);
     }
 
-    var mycard = document.querySelector("#mycard");
+    var mycard= document.querySelector("#mycard");
     for(var i in card){
         var opt = document.createElement("option");
         opt.setAttribute("value",card[i].bank);
@@ -35,67 +40,68 @@ window.onload=function(){
         mycard.appendChild(opt);
     }
 
-    var mybank = document.querySelector("#mybank");
+    var mybank= document.querySelector("#mybank");
     for(var i in bank){
         var opt = document.createElement("option");
         opt.setAttribute("value",bank[i].bank);
         opt.innerText=bank[i].name;
         mybank.appendChild(opt);
     }
- 
-    // 결제방법중에서 카드선택이냐 현금 선택이냐
+//결제방법 중에서  카드 선택이냐 현금 선택이냐
     var payment = document.getElementsByName("payment");
     payment[0].addEventListener("click",function(){
         way="현금";
-        document.querySelector("#mycard").classList.add("hide");
+        document.querySelector('#mycard').classList.add("hide");
     });
-    payment[1].addEventListener("click",function(){
-        document.querySelector("#mycard").classList.remove("hide");
+    payment[1].addEventListener("click",function(){ 
+        document.querySelector('#mycard').classList.remove("hide");
         way="카드";
     });
-
-    // 수입중에서 은행입금이냐 현금받았냐
+// 수입 중에서  은행 입금이냐 현금 받았냐
     var income_method = document.getElementsByName("income_method");
     income_method[0].addEventListener("click",function(){
-        document.querySelector("#mybank").classList.remove("hide");
+        document.querySelector('#mybank').classList.remove("hide");
         way="계좌";
     });
-    income_method[1].addEventListener("click",function(){
-        document.querySelector("#mybank").classList.add("hide");
+    income_method[1].addEventListener("click",function(){ 
+        document.querySelector('#mybank').classList.add("hide");
         way="현금";
     });
 
-    // 수입버튼 클릭시
+ //수입 버튼클릭시
     var income_bt = document.querySelector("#income_bt");
     income_bt.addEventListener("click",function(){
         var ex = document.getElementsByClassName("expense")[0];
         var inc = document.getElementsByClassName("income")[0];
         ex.classList.add("hide");
         inc.classList.remove("hide");
-        isIncome =true; //수입 클릭 했으니까
-    });
+        isIncome=true; // 수입 클릭했으니까
+    })
 
-    //지출버튼 클릭시
+    //지출버튼클릭시
     var expense_bt = document.querySelector("#expense_bt");
     expense_bt.addEventListener("click",function(){
         var ex = document.getElementsByClassName("expense")[0];
         var inc = document.getElementsByClassName("income")[0];
         ex.classList.remove("hide");
         inc.classList.add("hide");
-        isIncome=false;//지출 클릭 했으니까
+        isIncome=false; // 지출 클릭했으니까
     });
-    //등록버튼 클릭시
-    document.querySelector("#write_bt").addEventListener("click",add);
+
+
+    // 등록 버튼 클릭시
+    document.querySelector("#write_bt").addEventListener("click", add);
 }
+
 function add(){
-    var wday = document.querySelector("#wdate").value;
-    wday = wday.split("T")[0]+" "+wday.split("T")[i];
-    var money =document.querySelector("#money").value;
-    var cate = document.querySelector("#wcategory");
-    cate = cate.options[cate.selectedIndex].value;   
-    var detail = document.querySelector("#wdetail").value;
-    if(way!="현금"){ //계좌나 카드를 선택했다면
-        if(isIncome){ //수입을 선택했다면
+    var wday = document.querySelector("#wdate").value;  // 선택한 날짜
+    wday = wday.split("T")[0]+" "+wday.split("T")[1];
+    var money = document.querySelector("#wmoney").value; // 입력한 금액
+    var cate = document.querySelector("#wcategory"); 
+    cate = cate.options[cate.selectedIndex].value;   //선택한 분류
+    var detail = document.querySelector("#wdetail").value; //입력한 내용
+    if(way!="현금"){  // 계좌나 카드를 선택했다면
+        if(isIncome){  // 수입을 선택했다면
             var mybank = document.querySelector("#mybank");
             way += "-"+mybank.options[mybank.selectedIndex].value;
         }else{
@@ -103,8 +109,22 @@ function add(){
             way += "-"+mycard.options[mycard.selectedIndex].value;
         }
     }
-    account_update(money);//통장잔액변동
+    var balance = account_update(parseInt(money)); //통장 잔액 변동 
+
+    keep.push( new house(wday,money,cate, detail, way, isIncome?"수입":"지출", balance )  );
+
+    console.log(keep);
 }
+
 function account_update(money){
-    
+    if(way==="현금") return;
+    var use_bank = way.split("-")[1];
+    for(var i=0; i<bank.length; i++){
+        if( bank[i].bank=== use_bank ){  // 사용한 은행 찾기
+            if(isIncome)
+                return bank[i].money += money;
+            else
+                return bank[i].money -= money;
+        }
+    }
 }
