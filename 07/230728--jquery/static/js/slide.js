ef8850bfb29caef0f96c9e86ad7233a6ff1731f0
dlaalswl0313 <dlaalswl0313@naver.com>
2023-07-31 08:51:08 +0900

    230728

--- 07/230728--jquery/static/js/slide.js
+++ 07/230728--jquery/static/js/slide.js
@@ -1,14 +1,15 @@
 $(function(){
-    for(var i=1; i<=5; i++){
+
+    for(var i=1; i<=5; i++){//5장 제한
         $("#slide").append("<li><img src='https://loremflickr.com/1000/400?random="+i+"'></li>");
     }
+
     $(".left").click(function(){
-        if(parseInt($("#slide").css("left"))==0) $("#slide").css("left","-5000px");//왼쪽 값이 얼마인지 나옴, 단위까지 나옴
-        $("#slide").animate({left:"+= 1000px"});
+        if(  parseInt($("#slide").css("left")) == 0 ) $("#slide").css("left","-5000px");
+        $("#slide").animate({ left:"+=1000px"});
     });
     $(".right").click(function(){
-        if(parseInt($("#slide").css("left"))-=4000) $("#slide").css("left","1000px");//왼쪽 값이 얼마인지 나옴, 단위까지 나옴
-        $("#slide").animate({left:"-= 1000px"});
+        if(  parseInt($("#slide").css("left")) == -4000  ) $("#slide").css("left","1000px");
+        $("#slide").animate({ left:"-=1000px"});
     });
-    
 });
