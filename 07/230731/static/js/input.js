432c760cad10e00a3224c6b47cd1063a8613a712
dlaalswl0313 <dlaalswl0313@naver.com>
2023-07-31 14:21:09 +0900

    230731

--- 07/230731/static/js/input.js
+++ 07/230731/static/js/input.js
@@ -8,7 +8,18 @@ $(function(){
         $("#setImage").click(function(){
             var data = $("#imageName").val();
             
-            $("#gallery").append("<img src='"+data+"'>");
+            $("#gallery").append("<div class='img_box'><img src='"+data+"'></div>");
         });
+        $("#3n").click(function(){//한 줄에 3개씩
+            //$("#gallery").css("grid-template-columns:","repeat(3,1rf)");
+            $("#gallery").removeClass("grid-4n");//1줄에 4개씩 나오는 걸 지우고
+            $("#gallery").addClass("grid-3n");//한 줄에 3개씩 나오는 걸로 바뀜
+        });
+        $("#4n").click(function(){
+            $("#gallery").removeClass("grid-3n");
+            $("#gallery").addClass("grid-4n");
+        });
+       
     });
+    //https://loremflickr.com/1200/400?cat
 });
