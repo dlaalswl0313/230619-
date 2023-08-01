4fb4b943f21ec823fd736ac384586a47b806c758
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-01 17:30:59 +0900

    230701

--- 08/230801/static/js/text.js
+++ 08/230801/static/js/text.js
@@ -1,12 +1,15 @@
 $(function(){
     $("#keyword").on("keyup",function(){
         var word = $(this).val();
-       
+        
     });
     $("#category").on("change",function(){
-        $(".story").filter(function(){
+        $(".story h3").filter(function(){
             var isSearch=$(this).indexOf("연애소설") > -1;
             $(this).parent().toggle(isSearch);
         });
+        //if,switch냐 조건문필요함
     });
 });
+
+const cate =["all","romance","novel","poetry","proverb","diary"];
