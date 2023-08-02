4aa858ab421f66459053f13f09a369cc130d68c7
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-02 17:29:36 +0900

    230802

--- 08/230802/static/js/chat.js
+++ 08/230802/static/js/chat.js
@@ -1,6 +1,17 @@
+let re =0;
 $(function(){
-
+    $.getJSON("http://krdrive.ipdisk.co.kr:8000/test/data.php", function(){
+        chat_show(data);
+    });
+    re=setTimeout(function(){
+        location.reload();
+    },10000);
 });
+$(document).op("keyup",function(){
+   if(re!=0)
+        clearTimeout(re);
+});
+
 function send(){
     var send_data={ writer:$("#writer").val(), content:$("#content").val()};//json 변환이 필요없다.
     send_data = JSON.stringify(send_data);
@@ -10,11 +21,28 @@ function send(){
     
     xmlHttp.open("POST","http://krdrive.ipdisk.co.kr:8000/test/data.php");//get 인지 post인지 정하고 작성
     xmlHttp.onload=function(){
-        alert(this.response);
+        var data = this.response;
+        if(data==="fail")
+            alert("다시 입력하세요");
+        else  chat_show(JSON.parse(data));    
     };
     xmlHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlHttp.send("x="+send_data);
     //send() - 서버에 데이터 보내는 곳
+    re=setTimeout(function(){
+        location.reload();
+    },10000);
+}
+function chat_show(data){
+    var out="";
+    $.each(data,function(i,item){
+        out += "<li class=chat><div class='write_info'><b class='name'>"+item.writer+
+        "</b><span class='date'>"+item.date+"</span></div><div class='content'>"+item.content+
+        "</div></li>";
+    });
 
-
+    $("#chat_list").html(out);
+    $("#content").val('');
+    $("#content").focus();
+   
 }
