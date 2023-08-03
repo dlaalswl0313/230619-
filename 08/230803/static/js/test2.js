106ff450d6f3cb148d0334a10cdc5d49cf080fd2
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-03 13:48:17 +0900

    230803

--- 08/230803/static/js/test2.js
+++ 08/230803/static/js/test2.js
@@ -1,31 +1,36 @@
 $(function(){
     $.getJSON("https://jsonplaceholder.typicode.com/users",function(data){
         $.each(data,function(i,item){
-            $(".name").eq(i).data("id",item.id);
+            $(".name").eq(i).data('id',item.id);
             $(".name").eq(i).text(item.name);
-            //$("#person_wrap").append("<div class='name'>"+item.name+"</div>");
+            // $("#person_wrap").append("<div class='name' data-id='"+item.id+"'>"+item.name+"</div>");
+        });   
+    });    
+     $(".name").click(function(){
+        var id= $(this).data("id");
+         $.getJSON("https://jsonplaceholder.typicode.com/users?id="+id,function(data){
+            data = data[0];
+            $("#info_name").text(data.name);
+            $("#info_username").text(data.username);
+            $("#info_email").text(data.email);
+            $("#info_street").text(data.address.street);
+            $("#info_suite").text(data.address.suite);
+            $("#info_city").text(data.address.city);
+            $("#info_zipcode").text(data.address.zipcode);
+            $("#info_geo").text(data.address.geo.lat+","+data.address.geo.lng);
+            $("#info_phone").text(data.phone);
+            $("#info_website").text(data.website);
+            $("#info_companyName").text(data.company.name);
+            $("#info_companyBs").text(data.company.bs);
+
+            $("#modal").show();   
+            $("#modal_close").click(function(){
+                 $("#modal").hide();
+                });
+                // console.log(data[0]);
+            });
         });
-    });
-    $(".name").click(function(){
-        var id = $(this).data("id");
-    $.getJSON("https://jsonplaceholder.typicode.com/users"+id,function(data){
-         //console.log(data[0]);
-         data = data[0];//객체는 인덱스 없음
-         $("#info_name").text(data.name);
-         $("#info_username").text(data.username);
-         $("#info_email").text(data.email);
-         $("#info_street").text(data.address.street);
-         $("#info_suite").text(data.address.suite);
-         $("#info_city").text(data.addrerss.city);
-         $("#info_zipcode").text(data.address.zipcode);
-         $("#info_phone").text(data.address.phone);
-         $("#info_website").text(data.address.website);
-         $("#info_companyHome").text(data.address.companyHome);
-         $("#info_companyBs").text(data.address.companyBs);
-       });   
-    });
+        
     
-    $("#modal_close").click(function(){
-        $("#modal").hide();
+    
     });
-});
