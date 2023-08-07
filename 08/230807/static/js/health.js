77b27e3091b24bff07398e9be51e951ddd983e99
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-07 17:29:43 +0900

    230807

--- 08/230807/static/js/health.js
+++ 08/230807/static/js/health.js
@@ -8,8 +8,17 @@ $(function(){
         const data_list = data.records;
         var task = new Array();
         $.each(data_list, function(i,item){
-            task.push(item.건강증진업무내용);
-            console.log(item.건강증진업무내용);
+            $("#section").append("<div class='item_short'><div class='item_image'><img src='https://loremflickr.com/200/200/health?random="+i+"'></div>"+
+            "<div class='item_detail_box'><ul class='item_detail'>"+"<li>"+item.건강증진센터명+"</li><li>"+item.소재지도로명주소+"</li>"+
+            "<li>"+item.건강증진업무내용+"</li><li>"+item.건강증진센터구분+"</li>"+
+            "<li>"+item.운영기관명+"</li><li>"+item.운영기관전화번호+"</li></ul></div></div>");
         });
     });
+    
+     $("#searchWord").on("keyup",function(){
+         var word=$(this).val();
+         $("#section li").filter(function(){
+               $(this).toggle($(this).text().indexOf(word) > -1); 
+           });
+        });    
 });
