dae826ad5054098284e3c74b646a274d8c46f6f5
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-07 16:38:01 +0900

    230807

--- 08/230807/static/js/health.js
+++ 08/230807/static/js/health.js
@@ -4,4 +4,12 @@ $(function(){
         $(this).toggleClass("moreDown");
         $(this).toggleClass("moreUp");
     });
+    $.getJSON("./전국건강증진센터표준데이터.json",function(data){
+        const data_list = data.records;
+        var task = new Array();
+        $.each(data_list, function(i,item){
+            task.push(item.건강증진업무내용);
+            console.log(item.건강증진업무내용);
+        });
+    });
 });
