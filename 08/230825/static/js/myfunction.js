4bae4587511b1cec61b87643af9be77686bdff6b
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-28 08:51:44 +0900

    230825

--- 08/230825/static/js/myfunction.js
+++ 08/230825/static/js/myfunction.js
@@ -6,29 +6,29 @@ $(".pan").each(function(){
     $(this).css("background-size","cover");
     $(this).append("<div class='location_name'>"+(board_img[idx].split(".")[0])+"</div>");
 })
-    var tmp = 1;
-    $(".top").each(function(){
-        $(this).css("grid-area","top"+(tmp++));});var tmp = 1;
-    $(".bottom").each(function(){
-        $(this).css("grid-area","bottom"+(tmp++));});var tmp = 1;   
-    $(".sideR").each(function(){
-        $(this).css("grid-area","sideR"+(tmp++));});var tmp = 1;
-    $(".sideL").each(function(){
-        $(this).css("grid-area","sideL"+(tmp++));});
+    var tmp=1;
+    $(".top").each(function(){     $(this).css("grid-area","top"+(tmp++));   });
+    var tmp=1;
+    $(".sideR").each(function(){   $(this).css("grid-area","sideR"+(tmp++));   });
+    var tmp=1;
+    $(".bottom").each(function(){  $(this).css("grid-area","bottom"+(tmp++));   });
+    var tmp=1;
+    $(".sideL").each(function(){   $(this).css("grid-area","sideL"+(tmp++));   });
+
     
 
-    var areas='';
+     var areas ="";
     for(var i=1; i<=7; i++){
-        areas +="'";
-        for(var k=1; k<=9; k++){
-            if(i==1)areas += "top"+k+" ";
-            if(i==7)areas += "bottom"+(10-k)+" ";
-            if(i>1 && i<7) areas += (k==1)? "sideL"+(7-i)+" " : (k==9)?"sideR"+(i-1)+"  ":" center ";        
+          areas +="'";
+         for(var k=1; k<=9; k++){
+            if(i==1)areas +="top"+k+" ";
+            if(i==7)areas +="bottom"+(10-k)+" ";
+             if(i>1 && i<7) areas += (k==1)?"sideL"+(7-i)+" " : (k==9)?"sideR"+(i-1)+" ":" center ";
         }
-        areas +="'";
+            areas +="'";
     }
     console.log(areas);
-    $("#board_container").css("grid-template-areas", areas);
+    $("#board_container").css("grid-template-areas",areas);
 
 }
 const gcolor=["#F5B022","#EBC128","#DE8D2A","#F57422","#EB4F21"];
@@ -36,12 +36,12 @@ function draw(){
 //보드 그리기
     var gidx=Object.values(gamer)//참가자 번호 만 배열로 가져오기
     $.each(gidx,function(){
-        $(".pan").eq(0).append("<svg></svg>");
+        $(".pan").eq(0).append("<svg><circle cx='150' cy='100' r='50'>"+gcolor+"</circle></svg>");
     });
 }
 function setOpen(){
-    var child = window.open("set_gamer.html","참가자 설정",
-    'width=300,height=500,top=400,left=750');
+    var child = window.open("set_gamer.html",'참가자설정',
+    'width=300,height=300,top=400,left=750');
 }
 function dice_turn(){
     $(".dice").css('animation','turn 3s linear infinite');
