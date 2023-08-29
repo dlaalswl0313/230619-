12bddc25517680e45190d6f72f3c53d267f720ad
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-29 17:24:30 +0900

    nodejs

--- Nodejs web-nodejs2/App.js
+++ Nodejs web-nodejs2/App.js
@@ -14,13 +14,17 @@
        
 */
 var http= require('http');
-var fs = require('fs');
+var fs = require('fs'); //filesystem , 파일을 읽어올수있다.
 var tempUrl=require('url');
+var cookie = require('cookie');
+
+const data = JSON.parse(fs.readFileSync('./data/member.json','utf8'));
+//console.log(data);
 
 var app = http.createServer(function(request,response){
     var url = request.url;
     var query = tempUrl.parse(url,true).query;
-    console.log(query.part);//로그인해야 query.part에 login_check가 뜬다, 아니면 undefined
+    //console.log(query.part);//로그인해야 query.part에 login_check가 뜬다, 아니면 undefined
 
     if(query.part ==undefined){
         if(request.url=='/')//url이 일치해야지만 열림
@@ -31,18 +35,45 @@ var app = http.createServer(function(request,response){
             url='/src/question.html';
         if(request.url=='/login')
             url='/src/login.html';
+         response.writeHead(200);
     }else{
-        var page=query.part;
-        url='/src/'+page+'.html';
+        var page = query.part;
+        var isLogin ='false';
+        var id='';
+        if(page=='login_input'){
+            for(var i in data){
+                if(data[i].sdmId === query.sdmId && data[i].sdmPw ===query.sdmPw){
+                    isLogin='true';
+                    id=query.sdmID;
+                    break;
+                    //아이디, 비번 일치하면 쿠키 생성
+                }
+            }
+            url='/src/'+page+'.html';
+        }
+        
     }
+    response.writeHead(200,{
+        'Set-Cookie':['isLogin='+isLogin,'id='+id]
+    });
+    
         // if(request.url=='login_check')
         //     url='/src/login_check.html';
 
-        if(request.url =='/favicon.ico'){
-            return response.writeHead(404);
-        }
-    response.writeHead(200);
+    if(request.url =='/favicon.ico'){
+        return response.writeHead(404);
+    }
+    //console.log(request.headers.cookie);//내 pc에 저장되어있는 쿠키를 가져올수있다.
+    //var cookies = {};
+    //cookies = cookie.parse(request.headers.cookie);
+    //console.log(cookies.id);
+    
     response.end(fs.readFileSync(__dirname+url));
+    // response.writeHead(200,{
+    //     'Set-Cookie':['id=sky','pw=1234']// 'Set-Cookie':['키=값']
+    // });
+    // //response.end(fs.readFileSync(__dirname+url));
+    // response.end('aa');
 });
 app.listen(3000);
 
