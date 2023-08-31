1e4ab581b9739e4d35bd534a33b5ff52ea4e8858
dlaalswl0313 <dlaalswl0313@naver.com>
2023-08-31 17:25:51 +0900

    nodejs

--- web-nodejs3/App.js
+++ web-nodejs3/App.js
@@ -2,37 +2,60 @@ const http= require('http');
 const fs =require('fs');
 const url=require('url');
 const template = require('./lib/template.js');
+const dataParse = JSON.parse(fs.readFileSync('./lib/page.json','utf8'));
 
 const app = http.createServer(function(request, response){
     const pageURL=request.url;
     const query = url.parse(pageURL,true).query;// 쿼리 스트링
     const path = url.parse(pageURL,true).pathname; //루트도메인 뒤 주소
-
-    if( path ==="/"){
-        fs.readFileSync('./lib/page.json', 'utf8',function(){
+    if(path.indexOf(".")==-1){
+        var html='';
+        if( path ==="/"){
+            html = template.homeHTML(dataParse.main, dataParse.login_before);
+        }
+        if(path==='/login'){
+            html = template.loginHTML(dataParse.main);
+        }
+        if(path==='/sign'){
+            html = template.signupHTML(dataParse.main, dataParse.sign);
+        }
+        if(path==='/qs'){
+            var qdata = JSON.parse(fs.readFileSync('./lib/question.json','utf8'));
+            html = template.questionHTML(dataParse.main,dataParse,login_before,qdata);
+        }
+        response.writeHead(200);
+        response.write(html);
+        response.end();
+    }
+    if( path ==="/"){ //루트 경로 처리 로직, page.json 파일을 읽어와서 템플릿생성하고 응답
+        fs.readFile('./lib/page.json', 'utf8',function(err,data){
             const dataParse = JSON.parse(data);
-            const html = template.homeHTML(dataParse.main);
-            console.log(html);
+            const html = template.mainHTML(dataParse.main,dataParse.login_before);
+            //console.log(html);
             response.writeHead(200);
             response.write(html);
             response.end();
         });
     }
-    if(path.indexOf('.css')>-1){
+   
+    if(path.indexOf('.css')>-1){//css 파일 처리 로직, 요청된 css 파일을 읽어와서 응답
         var css_name=path.slice('/lib/'.length);
-        fs.readFile('./lib/'+css_name,function(){
+        fs.readFile('./lib/'+css_name,function(err,data){
             response.writeHead(200);
             response.write(data);
             response.end();
         });
     }
-    if(path.indexOf('/image')>-1){
+    if(path.indexOf('/image')>-1){//이미지 파일 처리 로직, 요청된 이미지 파일을 읽어와서 응답
         var img_name = path.slice('/image/'.length);
-        fs.readFile('./image/'+img_name,function(){
-            response.writeHead(200);
+        fs.readFile('./image/'+img_name,function(err,data){
+            response.writeHead(200); 
             response.write(data);
             response.end();
         });
     }
 });
 app.listen(3000);
+ 
+
+
--- web-nodejs3/lib/template.js
+++ web-nodejs3/lib/template.js
@@ -1,44 +1,187 @@
 // template.js
 
-module.exports = {
-    homeHTML:function(main){
-        var menu = "";
-        for(var key of Object.keys(main.menu)){
-            menu+=`<li class="menu"><a href="/${key}">${main.menu[key]}</a></li>`;
-        }
+module.exports = { //객체형식,modul.exportss는 모듈에서 외부로 내보낼 항목 정의   
+        questionHTML:function(main,login,qsData){
+            var tag='';
+            for(var q of qsData){
+               tag += "<tr><td>"+q.id+"</td><td>"+q.title+"</td><td>"+q.write
+               +"</td><td>"+q.date+"</td><td>"+q.to+"</td></tr>";
+            }
 
-        return (`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>스드메의 모든것</title>
-    <link rel="stylesheet" href="./src/main.css">
-    <link rel="stylesheet" href="./src/index.css">
-
-</head>
-<body>
-    <div id="wrap">
-        <header id="header">
-            <div class="logo"><a href="/"><img src="./image/${main.logo}"></a></div>
-            <nav class="nav">
-                <ul class="menuList">
-                   ${menu}
-                </ul>
-            </nav>
-        </header>
-        <main id="main">
+            var qsHTML=`
             <section id="content">
-                <img src="./image/${main.main_img}">
+                <div id="qsList">
+                    <div class="qsTitle">
+                        <h2>문의</h2>
+                        <a href="javascript:questionWrite();">문의하기</a>
+                    </div>
+                    <div class="search_wrap">
+                        <input type="text" name="word" id="word">
+                    </div>
+                    <div class="qsList_box">
+                        <table>
+                            <thead>
+                                <th>번호</th><th>제목</th><th>작성자</th>
+                                <th>작성일</th><th>답변</th>
+                            </thead>
+                            <tbody id="qs">${tag}</tbody>
+                        </table>
+                    </div>
+                </div>
             </section>
             <section id="side">
-                
-            </section>
-        </main>
-    </div>
-</body>
-</html>
-        `);
+                <div class="login_bt">
+                    <a href="/${login.url}">${login.text}</a>
+                </div>
+            </section>`;
+            return ( commonHTML(main,mainHTML,"question") );
+     },
+    mainHTML:function(main, login){
+    var mainHTML=` 
+        <section id="content">
+            <img src="./image/${main.main_img}">
+        </section>
+        <section id="side">
+            <div class="login_bt">
+                <a href="/${login.url}">${login.text}</a>
+            </div>
+        </section>`;
+        return ( commonHTML(main,mainHTML,"index") );
+    },
+    
+    loginHTML:function(main){
+        var loginHTML=`
+        <section id="content">
+                <h3>스드메 로그인</h3>
+               <div class="login_wrap">
+                    <form name="loginf" id="loginf" method="get" action="/">
+                        <input type="hidden" name="part" value="login_check">
+                        <div class="login_box">
+                                <div class="login_input">
+                                    <input type="text" name="sdmId" id="sdmId">
+                                    <label for="sdmId">ID</label>
+                                </div>
+                                <div class="login_input">
+                                    <input type="password" name="sdmPw" id="sdmPw">
+                                    <label for="sdmPw">PW</label>
+                                </div>
+                                <div class="login_bt">
+                                    <a href="javascript:login();">LOGIN</a>
+                                </div>
+                        </div>
+                    </form>
+                    <div class="joinAfind">
+                        <p class="find">아이디/비밀번호 찾기</p>
+                        <p class="join"><a href="/sign">회원가입</a></p>
+                    </div>
+               </div>
+            </section>`;
+        return (commonHTML(main,loginHTML,"login"));
+    },
+    signupHTML:function(main){
+        var signupHTML=`
+        <section id="content">
+                <h3>스드메 회원가입</h3>
+                <table id="list">
+                    <tr>
+                        <td>
+                            <label>아이디</label>
+                        </td>
+                        <td>
+                            <input type="text" name="id" id="id">
+                            <button id="ck">중복확인</button>
+                        </td>
+                    </tr>  
+                    <tr></tr> 
+                        <td>
+                            <label>비밀번호</label>
+                        </td>
+                        <td>
+                            <input type="password" name="pw" id="pw">
+                        </td> 
+                    <tr>                      
+                        <td>
+                            <label>비밀번호 확인</label>
+                        </td>  
+                        <td> 
+                            <input type="password" name="pwck" id="pwck">
+                        </td>
+                    </tr> 
+                    <tr> 
+                        <td>
+                            <label>이름</label>
+                        </td>
+                        <td>
+                            <input type="text" name="name" id="name">
+                            
+                        </td>
+                    </tr>
+                    <tr> 
+                        <td>
+                            <label>전화번호</label>
+                        </td>
+                        <td>
+                            <input type="number" name="tel" id="tel">
+                        </td>
+                    </tr>
+                    <tr> 
+                        <td>
+                            <label>주소</label>
+                        </td>
+                        <td>
+                            <input type="text" name="add" id="add">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>결혼예정일</label>
+                        </td>
+                        <td>
+                            <input type="date" name="wDate" id="wDate">
+                        </td>
+                    </tr>
+                </table>
+                <div id="bt_list">
+                    <button id="send">등록</button>
+                    <button id="cencal">취소</button>
+                </div>
+            </section>`;
+        return (commonHTML(main,signupHTML,"sign")); 
+        //commonHTML 함수를 호출하는 부분, main 전달되는 메인정보객체, 회원가입페이지의 html내용, 문자열로 css파일을 표현
     }
+}
+
+function commonHTML(main,html,css){//commonHTML 문서의 공통구조 생성(메인,html,css를 받아 동적으로 생성)
+    var menu = "";
+    for(var key of Object.keys(main.menu)){//${}데이터 삽입
+        menu+=`<li class="menu"><a href="/${key}">${main.menu[key]}</a></li>`;//${main.menu[key]} 메뉴이름삽입 menu변수에 누적
+    }
+    return (`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>스드메의 모든것</title>
+    
+        <link rel="stylesheet" href="./lib/main.css">
+        <link rel="stylesheet" href="./lib/${css}.css">
+    </head>
+    <body>
+        <div id="wrap">
+            <header id="header">
+                <div class="logo"><a href="/"><img src="./image/${main.logo}"></a></div>
+                <nav class="nav">
+                    <ul class="menuList">
+                       ${menu}
+                    </ul>
+                </nav>
+            </header>
+            <main id="main">
+                ${html}
+            </main>
+        </div>
+    </body>
+    </html>        
+    `);
 }
