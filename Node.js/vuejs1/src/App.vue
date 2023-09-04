8854df1681f53a5af0433b0d868e4ea11b0bb731
dlaalswl0313 <dlaalswl0313@naver.com>
2023-09-04 14:13:34 +0900

    vuejs

--- Node.js/vuejs1/src/App.vue
+++ Node.js/vuejs1/src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header id="header">
+    <div class="logo"><a href="/"><img src="./assets/logo.png"></a></div>
+        <nav class="nav">
+             <ul class="menuList">
+               <li class="menu"><a href="/studio">스튜디오</a></li>
+               <li class="menu"><a href="/dress">드레스</a></li>
+               <li class="menu"><a href="/makeup">메이크업</a></li>
+               <li class="menu"><a href="/wedding">예식장</a></li>
+               <li class="menu"><a href="/qs">Q&A</a></li>
+            </ul>
+        </nav>
+      </header>
+  <MyHome/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHome from './components/MyHome.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyHome
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    padding:0;
+    margin:0;
+    box-sizing: border-box;
+}
+#app{
+    width:1000px;
+    margin:0 auto;
+    position:relative;
+}
+#header{
+    width:100%;
+    padding:30px 0;
+    display:flex; 
+    justify-content: space-between;
+    align-items: center;
+}
+#header .logo{width:150px;}
+#header .logo img{width:100%;}
+#header .nav{width:850px;}
+#header .nav .menuList{
+    list-style: none;
+    width:100%; 
+    display:flex;
+    justify-content: space-evenly;
+    font-size:20px;
+}
+#header .nav .menuList .menu a{
+    color:black;
+    text-decoration: none;
+    font-weight:800;
+}
+
+#main{
+    display:grid;
+    grid-template-columns: 4fr 1fr;
+}
+#main img{
+    width:100%;
+}
+#side .login_bt{
+    padding:5px 10px;
+}
+#side .login_bt a{
+    color:white;
+    text-decoration:none;
+    background:#A64444;
+    padding:10px 0;
+    width:100%;
+    font-size:20px;
+    font-weight: 800;
+    display:inline-block;
+    border-radius: 5px;
+    text-align: center;
 }
 </style>
---
+++ Node.js/vuejs1/src/components/MyHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <!--주석-->
+    <main id="main">
+            <section id="content">
+                <img src="../assets/main.jpg">
+            </section>
+            <section id="side">
+                <div class="login_bt"><a href="/login">로그인</a></div>
+            </section>
+        </main>
+</template>
+<script>
+/* 주석 */ 
+//주석 
+export default {
+  name: 'MyHome',
+}
+</script>
+
