130e33f19187ed020f487cbe187657a1209b87cb
dlaalswl0313 <dlaalswl0313@naver.com>
2023-09-27 09:33:46 +0900

    project

--- 0.Attackmo/pj_second/src/views/login.vue
+++ 0.Attackmo/pj_second/src/views/login.vue
@@ -1,5 +1,4 @@
 <template>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
       <div id="wrap">
         <div id="login_box">
             <div id="title">
@@ -10,14 +9,14 @@
                 <input type="password" placeholder="비밀번호">
                 <button id="login_bt">로그인</button>  
                 <div id="find">
-                    <label>ID/PW 찾기</label>
-                    <label>회원가입</label>
+                    <label><a href="">ID/PW 찾기</a></label>
+                    <label><a href="">회원가입</a></label>
                 </div>
             </div>
                 <div id="social_login">
-                    <a href="" id="facebook" class="social_bt"><i class="bi bi-facebook"></i></a>
-                    <a href="" id="twitter" class="social_bt"><i class="bi bi-twitter"></i></a>
-                    <a href="" id="instagram" class="social_bt"><i class="bi bi-instagram"></i></a>
+                    <a href="" id="facebook" class="social_bt"><img src="../images/facebook.png"></a>
+                    <a href="" id="twitter" class="social_bt"><img src="../images/twitter.png"></a>
+                    <a href="" id="instagram" class="social_bt"><img src="../images/instagram.png"></a>
                 </div>
         </div>
     </div>
@@ -76,10 +75,9 @@
         display: flex;
         width: 831px;
         height: 85px;
-        padding: 25px 368px;
         justify-content: center;
         align-items: center;
-        gap: 10px;
+        padding: 25px 368px;
         font-size: 23px;
         border-radius: 8px;
         background: #F9C041;
@@ -89,20 +87,24 @@
     #social_login{
         display: flex;
         align-items: flex-start;
-        gap: 87px;
+        gap: 87px;   
     }
     .social_bt{
-        width: 106.013px;
-        height: 106.013px;
-        border: 1px solid black;
-        text-align: center;
-        font-size: 32px;
-        border-radius:100%;
+        font-size: 34px;
+        width: 130px;
+        margin-bottom:40px;
     }
+    .social_bt img{ 
+        width: 130px;
+        height: 130px;
+        margin:-11px;
+    }
+    #instagram img{width:106.013px;height: 106.013px;margin:0px;}
     #find{
         display: flex;
         align-items: flex-start;
         gap: 11px;
     }
     #find label{font-size: 17px;color:#7A7979;}
+    a{text-decoration: none;}
 </style>
